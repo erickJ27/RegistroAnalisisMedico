77989d439b86596a9312d389346f75aa72e018f5
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-06-11 21:12:49 -0400

    AnalisisBLL creada

---
+++ BLL/AnalisisBLL.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnalisisMedico.Entidades;
+using AnalisisMedico.DAL;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace AnalisisMedico.BLL
+{
+    public class AnalisisBLL
+    {
+        public static bool Guardar(Analisis analisi)
+        {
+            bool paso = false;
+
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.Analisi.Add(analisi) != null)
+                    paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+        public static bool Modificar(Analisis analisi)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                var Anterior = db.Analisi.Find(analisi.AnalisisId);
+                foreach (var item in Anterior.Resultado)
+                {
+                    if (!analisi.Resultado.Exists(d => d.Id == item.Id))
+                        db.Entry(item).State = EntityState.Deleted;
+                }
+                db.Entry(analisi).State = EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                Analisis analisi = contexto.Analisi.Find(id);
+
+                contexto.Analisi.Remove(analisi);
+
+                if (contexto.SaveChanges() > 0)
+                {
+                    paso = true;
+                }
+                contexto.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+        public static Analisis Buscar(int id)
+        {
+            Contexto db = new Contexto();
+            Analisis analisi = new Analisis();
+            try
+            {
+                analisi = db.Analisi.Find(id);
+
+                analisi.Resultado.Count();
+            }
+            catch
+            {
+                throw;
+
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return analisi;
+        }
+        public static List<Analisis> GetList(Expression<Func<Analisis, bool>> analisi)
+        {
+            List<Analisis> Lista = new List<Analisis>();
+            Contexto db = new Contexto();
+
+            try
+            {
+                Lista = db.Analisi.Where(analisi).ToList();
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnalisisMedico.Entidades;
+using System.Data.Entity;
+
+namespace AnalisisMedico.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Analisis> Analisi { get; set; }
+        public DbSet<TipoAnalisis> TipoAnali { get; set; }
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
