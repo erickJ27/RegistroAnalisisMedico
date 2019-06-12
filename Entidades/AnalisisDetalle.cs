ba52287babbfe5d3316a919135e841bdb394f103
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-06-11 20:46:10 -0400

    Creadas las Entidades AnilisisDetalle y TipoAnalisis

---
+++ Entidades/AnalisisDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisMedico.Entidades
+{
+    public class AnalisisDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int AnalisisId { get; set; }
+
+        public string Resultado { get; set; }
+
+        public AnalisisDetalle()
+        {
+            Id = 0;
+            AnalisisId = 0;
+            Resultado = string.Empty;
+        }
+    }
+}
---
+++ Entidades/TipoAnalisis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisMedico.Entidades
+{
+    public class TipoAnalisis
+    {
+        [Key]
+        public int TipoId { get; set; }
+        public string Descripcion { get; set; }
+
+        public TipoAnalisis()
+        {
+            TipoId = 0;
+            Descripcion = string.Empty;
+
+        }
+    }
+}
